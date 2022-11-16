d4f39a1f4c9e4d5e3afcb019c7ca5f0d1bd6b305
Batystyle1 <sbakytbekov7@gmail.com>
2022-11-16 11:32:52 +0600

    Initial commit

---
+++ src/gdsc/java/Solution5.java
@@ -0,0 +1,21 @@
+package gdsc.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Solution5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for (int i = 1; i<11; i++){
+
+            System.out.println(N + " " + "x"+" "+i +" = "+ N * i);
+        }
+
+
+        System.out.println(N);
+        bufferedReader.close();
+}}
