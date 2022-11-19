d1d1082ec2153d117fa05cb2ddf0686adf121ef6
Batystyle1 <sbakytbekov7@gmail.com>
2022-11-20 03:44:22 +0600

    Initial commit

--- src/gdsc/java/Solution7.java
+++ src/gdsc/java/Solution7.java
@@ -1,4 +1,28 @@
 package gdsc.java;
+import java.lang.Math.*;
+import java.util.Scanner;
 
 public class Solution7 {
+
+public static void main(String[] args) {
+
+
+    Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int ans = a;
+            for (int j = 0; j < n; j++) {
+                ans += Math.pow(2 , j)*b;
+                System.out.println(ans + " ");
+
+            }
+
+
+
+        }
+        in.close();
+}
 }
